cb838993e660653c4313567f997dec9f70750a96
Axel <axel.olsson@gmail.com>
2018-02-17 01:41:11 -0500

    Using applyWithFunction (#1)

--- src/jsxTypedClassnamesRule.ts
+++ src/jsxTypedClassnamesRule.ts
@@ -1,45 +1,69 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
 
-
 export class Rule extends Lint.Rules.AbstractRule {
-  public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    return this.applyWithWalker(new TypedClassNamesWalker(sourceFile, this.getOptions()));
-  }
-}
 
-// https://github.com/reworkcss/css
-// https://github.com/zignd/HTML-CSS-Class-Completion/blob/b8f80901b25950375a90efdde67b18b281201777/src/parse-engines/common/css-class-extractor.ts
+  static metadata: {
+    ruleName: 'jsx-typed-classnames',
+    type: 'maintainability',
+    description: '',
+    optionsDescription: '',
+    options: null,
+    typescriptOnly: false
+  };
+
+  constructor(options: Lint.IOptions) {
+    super({ ...options, ruleSeverity: 'warning' });
+  }
+
+  public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+    return this.applyWithFunction(sourceFile, walk);
+  }
+
+}
 
 const KNOWN_CLASS_NAMES = new Set<string>([
   'text-blue-dark'
 ]);
 
-class TypedClassNamesWalker extends Lint.RuleWalker {
+function walk(ctx: Lint.WalkContext<void>) {
 
-  public visitJsxAttribute(node: ts.JsxAttribute) {
-    if (node.initializer
-      && node.initializer.kind === ts.SyntaxKind.StringLiteral
-      && node.name.text === 'className') {
+  const isClassNameJsxAttribute =
+    (attrib: ts.JsxAttribute): attrib is ts.JsxAttribute & { initializer: ts.StringLiteral } =>
+      attrib.initializer
+        ? attrib.initializer.kind === ts.SyntaxKind.StringLiteral && attrib.name.text === 'className'
+        : false;
 
-      const classNames = node.initializer.getText().slice(1, -1).split(' ');
-      const nbOfClassNames = classNames.length;
-
-      let startPos = node.initializer.getStart() + 1;
-
-      for (let i = 0; i < nbOfClassNames; ++i) {
-        const className = classNames[i];
-        const width = className.length;
-
-        if (width && !KNOWN_CLASS_NAMES.has(className)) {
-          this.addFailureAt(startPos, width, `Unknown class name ${className}`);
-        }
-
-        startPos += width + 1;
-      }
-
+  const checkJsxAttribute = (attrib: ts.JsxAttribute) => {
+    if (!isClassNameJsxAttribute(attrib)) {
+      return;
     }
 
-    super.visitJsxAttribute(node);
-  }
+    const classNameText = attrib.initializer.text;
+    const startPos = attrib.getEnd() - classNameText.length - 1;
+
+    const classNames = classNameText.split(' ');
+    const nbOfClassNames = classNames.length;
+
+    for (let i = 0, currentPos = 0; i < nbOfClassNames; ++i) {
+      const className = classNames[i];
+      const width = className.length;
+
+      if (width && !KNOWN_CLASS_NAMES.has(className)) {
+        ctx.addFailureAt(startPos + currentPos, width, `Unknown class "${className}"`);
+      }
+
+      currentPos += width + 1;
+    }
+  };
+
+  const cb = (node: ts.Node): void => {
+    if (node.kind === ts.SyntaxKind.JsxAttribute) {
+      checkJsxAttribute(node as ts.JsxAttribute);
+    }
+
+    return ts.forEachChild(node, cb);
+  };
+
+  return ts.forEachChild(ctx.sourceFile, cb);
 }
